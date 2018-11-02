0208eef8f01308c75448be7533e5cd40957f2447
Shounak Itraj <sitraj@qualys.com>
2018-11-02 19:15:28 +0530

    Updated code to make it more readable

--- domparser.js
+++ domparser.js
@@ -1,32 +1,32 @@
 var node = document.documentElement
+
 function callnode(node) {
-        var result = {tag: node.nodeName}, nodes, i;
-        if (node.childElementCount) {
-                result.children = [];
-                for (i = 0; nodes = node.children[i]; i++) {
-                        for (k=0; k<node.childNodes.length; k++)
-                        {
-                                if (node.childNodes[k].nodeType === 3 ||  node.childNodes[k].nodeType === 8)
-                                {
-                                        result[node.childNodes[k].nodeValue] = node.childNodes[k].nodeValue
-                                }
-                        }
-                        result.children.push(callnode(nodes));
+    var result = {
+            tag: node.nodeName
+        },
+        nodes, i;
+    if (node.childElementCount) {
+        result.children = [];
+        for (i = 0; nodes = node.children[i]; i++) {
+            for (k = 0; k < node.childNodes.length; k++) {
+                if (node.childNodes[k].nodeType === 3 || node.childNodes[k].nodeType === 8) {
+                    result[node.childNodes[k].nodeValue] = node.childNodes[k].nodeValue
                 }
-
-        }
-        for (k=0; k<node.childNodes.length; k++)
-        {
-                if (node.childNodes[k].nodeType === 3 ||  node.childNodes[k].nodeType === 8)
-                {
-                        result[node.childNodes[k].nodeValue] = node.childNodes[k].nodeValue
-                }
+            }
+            result.children.push(callnode(nodes));
         }
 
-        for (i = 0; nodes = node.attributes[i]; i++) {
-                result[nodes.nodeName] = nodes.nodeValue;
+    }
+    for (k = 0; k < node.childNodes.length; k++) {
+        if (node.childNodes[k].nodeType === 3 || node.childNodes[k].nodeType === 8) {
+            result[node.childNodes[k].nodeValue] = node.childNodes[k].nodeValue
         }
-        return result;
+    }
+
+    for (i = 0; nodes = node.attributes[i]; i++) {
+        result[nodes.nodeName] = nodes.nodeValue;
+    }
+    return result;
 }
 tagresult = callnode(node)
 console.log(tagresult)
